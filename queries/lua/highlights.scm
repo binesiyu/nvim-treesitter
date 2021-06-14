46de21754f042701e55f9156a09cce92077d6340
yubin <1960028060@qq.com>
2021-06-15 06:35:44 +0800

    fix-lua

--- queries/lua/highlights.scm
+++ queries/lua/highlights.scm
@@ -51,11 +51,14 @@
 
 [
  "in"
- "local"
  "return"
  (break_statement)
  "goto"
 ] @keyword
+
+[
+ "local"
+] @type
 
 ;; Operators
 
@@ -103,34 +106,43 @@
 ] @punctuation.bracket
 
 ;; Variables
-(identifier) @variable
+; (identifier) @variable
 
 ;; Constants
 [
 (false)
 (true)
 ] @boolean
-(nil) @constant.builtin
+
+[
+(self)
+(nil)
+]@constant.builtin
+
 (spread) @constant ;; "..."
 
 ;; Functions
-(function [(function_name) (identifier)] @function)
+(function [(function_name) (identifier)] @function.macro)
 (function ["function" "end"] @keyword.function)
 
-(local_function (identifier) @function)
+(local_function (identifier) @function.macro)
 (local_function ["function" "end"] @keyword.function)
 
 (function_definition ["function" "end"] @keyword.function)
 
-(property_identifier) @property
-(method) @method
+; (property_identifier) @property
+; (method) @method
 
-(function_call (identifier) @function . (arguments))
-(function_call (field_expression (property_identifier) @function) . (arguments))
+(function_call (self) (method) @method . (arguments))
+(function_call (identifier) @method . (arguments))
+(function_call (field_expression (property_identifier) @method) . (arguments))
+
+(function_call (identifier) @text . (method) @method (arguments))
+(function_call (field_expression (property_identifier) @text) . (method) @method (arguments))
 
 ;; Parameters
-(parameters
-  (identifier) @parameter)
+; (parameters
+;   (identifier) @parameter)
 
 ;; Nodes
 (table ["{" "}"] @constructor)
@@ -139,7 +151,7 @@
 (number) @number
 (label_statement) @label
 ; A bit of a tricky one, this will only match field names
-(field . (identifier) @field (_))
+; (field . (identifier) @field (_))
 (shebang) @comment
 
 ;; Error
